60cce86e6ede546b31cf71352a60b59f4fdca163
EOMJI <71482583+bisous1519@users.noreply.github.com>
2022-10-10 16:30:18 +0900

    [Silver III] Title: N과 M (1), Time: 220 ms, Memory: 56056 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,34 +1,44 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+// 순열 (nPm)
 public class Main {
+
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer tokens;
+	static StringBuilder output = new StringBuilder();
 	
-	static int N;
-	static int M;
+	static int N, M;
 	
-	public static void permu(int nth, int[] choosed, boolean[] isVisited) {
+	public static void permu(int nth, int[] choosed, boolean[] visited) {
 		if(nth == M) {
 			for(int i=0; i<M; i++) {
-				System.out.print(choosed[i] + " ");
+				output.append(choosed[i] + " ");
 			}
-			System.out.println();
+			output.append("\n");
+			
 			return;
 		}
 		
 		for(int i=1; i<=N; i++) {
-			if(!isVisited[i]) {
-				isVisited[i] = true;
+			if(!visited[i]) {
+				visited[i] = true;
 				choosed[nth] = i;
-				permu(nth + 1, choosed, isVisited);
-				isVisited[i] = false;
+				permu(nth + 1, choosed, visited);
+				visited[i] = false;
 			}
 		}
 	}
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		tokens = new StringTokenizer(input.readLine());
+		N = Integer.parseInt(tokens.nextToken());
+		M = Integer.parseInt(tokens.nextToken());
+		
 		permu(0, new int[M], new boolean[N+1]);
+		
+		System.out.println(output);
 	}
-
 }
