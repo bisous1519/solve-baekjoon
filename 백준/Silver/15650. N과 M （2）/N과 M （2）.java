c09d4a568abfc0a05b4b6720f9a7f21a54797f3e
EOMJI <71482583+bisous1519@users.noreply.github.com>
2022-10-10 16:48:00 +0900

    [Silver III] Title: N과 M (2), Time: 76 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,29 +1,30 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 순열 (nPm) (오름차순)
 public class Main {
-
+	
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer tokens;
 	static StringBuilder output = new StringBuilder();
+
+	static int N, M;
 	
-	static int N;
-	static int M;
-	
-	public static void combi(int nth, int[] choosed, int start) {
+	public static void permu(int nth, int[] choosed, int start) {
 		if(nth == M) {
-			for(int a : choosed) {
-				output.append(a + " ");
+			for(int i=0; i<M; i++) {
+				output.append(choosed[i] + " ");
 			}
 			output.append("\n");
+			
 			return;
 		}
 		
 		for(int i=start; i<=N; i++) {
 			choosed[nth] = i;
-			combi(nth + 1, choosed, i + 1);
+			permu(nth + 1, choosed, i + 1);
 		}
 	}
 	
@@ -31,7 +32,9 @@ public class Main {
 		tokens = new StringTokenizer(input.readLine());
 		N = Integer.parseInt(tokens.nextToken());
 		M = Integer.parseInt(tokens.nextToken());
-		combi(0, new int[M], 1);
+		
+		permu(0, new int[M], 1);
+		
 		System.out.println(output);
 	}
 }
