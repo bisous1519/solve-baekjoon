1b2cb48edbe408f393b3735a78f36b07c633bfdb
EOMJI <71482583+bisous1519@users.noreply.github.com>
2023-01-22 01:39:09 +0900

    [Bronze IV] Title: 기찍 N, Time: 1596 ms, Memory: 32660 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Main {
+    
+    static Scanner sc = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        int N = sc.nextInt();
+        for(int i=N; i>=1; i--) {
+            System.out.println(i);
+        }
+    }
+}
