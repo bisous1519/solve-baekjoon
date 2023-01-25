d67dc6ad78b832617f716300f2313d0c152df903
EOMJI <71482583+bisous1519@users.noreply.github.com>
2023-01-25 20:54:14 +0900

    [Bronze V] Title: N 찍기, Time: 1544 ms, Memory: 35764 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main {
+    
+    public static void main(String[] agrs){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int n=1; n<=N; n++){
+            System.out.println(n);
+        }
+    }
+}
