d0d42341c80e037befd686ca4debd1f35d9c18b4
EOMJI <71482583+bisous1519@users.noreply.github.com>
2023-02-04 23:56:13 +0900

    [Bronze V] Title: 윤년, Time: 112 ms, Memory: 13020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+	
+	static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		int year = sc.nextInt();
+		
+		// 윤년
+		if(year%4==0 && year%100!=0
+			|| year%400==0) {
+			System.out.println(1);
+		}else {
+			System.out.println(0);
+		}
+	}
+}
