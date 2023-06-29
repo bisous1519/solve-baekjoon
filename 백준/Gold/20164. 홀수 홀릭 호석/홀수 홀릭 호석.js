178d61b88cf3e3b5aa91adaebd24a9492f2a0ca4
EOMJI <71482583+bisous1519@users.noreply.github.com>
2023-06-29 21:27:39 +0900

    [Gold V] Title: 홀수 홀릭 호석, Time: 128 ms, Memory: 9960 KB -BaekjoonHub

--- 백준/Gold/20164. 홀수 홀릭 호석/홀수 홀릭 호석.js
+++ 백준/Gold/20164. 홀수 홀릭 호석/홀수 홀릭 호석.js
@@ -4,25 +4,25 @@ let Max = 0;
 const countOddNum = (arr) => arr.filter((num) => num % 2 === 1).length;
 
 const recur = (N, cntOdd) => {
-  let arr = N.split('');
-  let len = arr.length;
+  const arr = N.split('');
+  const len = arr.length;
 
-  let sumCntOdd = cntOdd + countOddNum(arr);
+  const sumCntOdd = cntOdd + countOddNum(arr);
 
   if (len === 1) {
     Min = Math.min(Min, sumCntOdd);
     Max = Math.max(Max, sumCntOdd);
     return;
   } else if (len === 2) {
-    let temp = parseInt(arr[0]) + parseInt(arr[1]);
+    const temp = parseInt(arr[0]) + parseInt(arr[1]);
     recur(temp.toString(), sumCntOdd);
   } else if (len >= 3) {
     for (let i = 0; i < len - 2; i++) {
       for (let j = i + 1; j < len - 1; j++) {
         // 삼등분함
-        let num1 = parseInt(N.substring(0, i + 1));
-        let num2 = parseInt(N.substring(i + 1, j + 1));
-        let num3 = parseInt(N.substring(j + 1));
+        const num1 = parseInt(N.substring(0, i + 1));
+        const num2 = parseInt(N.substring(i + 1, j + 1));
+        const num3 = parseInt(N.substring(j + 1));
 
         // 세 개 수 만들어짐
         recur((num1 + num2 + num3).toString(), sumCntOdd);
