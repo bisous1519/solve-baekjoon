dc63e22bab0def8e2a39c0a76f026e7b4d6e5733
Eomji <71482583+bisous1519@users.noreply.github.com>
2024-02-24 16:13:01 +0900

    [Gold V] Title: 치킨 배달, Time: 220 ms, Memory: 12856 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.js
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.js
@@ -1,103 +1,89 @@
-// N*N 도시
-//   (1, 1) ~ (r, c)
-//   1*1 각 칸은 아래 중 하나
-//     - 0 : 빈 칸
-//     - 1 : 집
-//     - 2 : 치킨 집
+// 도시 N*N
+//   - 0 : 빈칸
+//   - 1 : 집
+//   - 2 : 치킨집
 // 치킨 거리
-//   : 집과 가장 가까운 치킨집 사이의 거리
-//   (r1, c1)과 (r2, c2) 사이의 거리는 |r1-r2| + |c1-c2|
-//   각 집은 치킨 거리를 가짐
+//   - 집과 가장 가까운 치킨집 사이의 거리
+//   - |r1 - r2| + |c1 - c2|
 // 도시의 치킨 거리
-//   : 모든 집의 치킨 거리의 합
-// 치킨 집 중 최대 M개를 고르고 나머지 치킨집은 모두 폐업
-// -> 도시의 치킨 거리가 가장 작게 되는!
-//
-// => 치킨집 위치를 리스트에 저장
-//    치킨집 고르는 조합! (1개 ~ M개)
-//    조합 고를 때 마다 도시의 치킨 거리를 구함?
+//   - 모든 집의 치킨 거리의 합
+// 가장 수익을 많이 낼 수 있는 치킨집 M개를 고르고 나머지는 모두 폐업
+// => 출력
+//    M개 고른 후 도시 치킨 거리의 최솟값
+// => 풀이
+//    - 모든 치킨집의 위치를 넣은 배열과, 모든 집 위치를 넣은 배열을 만듦
+//    - 치킨집 중 m개 고르고 
+//      -> 각 치킨집을 돌면서
+//      -> 치킨집에서 각 집까지의 거리들을 측정
+//      -> 각 집의 치킨 거리 갱신
+//      => 도시 치킨 거리 갱신
+//    - m===6 -> 치킨집갯수Cm * (m*50) = 1716 * 30 = 5만
 
-let Map;
 let N, M;
-let cList; // 치킨집 리스트
-let cLen;
-let hList; // 집 리스트
-let cityDis = Number.MAX_SAFE_INTEGER;
-const [BLANK, HOME, CHICKEN] = [0, 1, 2];
+let Map;
+let Chicken = [];
+let House = [];
+let chickenLen;
+let houseLen;
+let MinCityDis = Number.MAX_SAFE_INTEGER;
 
 const Loc = function(r, c) {
     this.r = r;
     this.c = c;
 }
 
-const combi = (nth, isSelected, m) => {
-    if(nth === cLen) {
-        if(0 < m && m <= M) {
-            // console.log(isSelected);
-            // 선택된 치킨집 좌표 리스트 만들기
-            let selectedClist = [];
-            isSelected.forEach((el, idx) => {
-                if(el) {
-                    selectedClist.push(cList[idx]);
-                }
-            })
+const measure = (selected) => {
+    let dis = new Array(houseLen).fill(Number.MAX_SAFE_INTEGER);
 
-            // 각 집마다 치킨거리 구해서 더하기
-            let sum = 0;
-            for(hLoc of hList) {
-                let min = Number.MAX_SAFE_INTEGER;
-                selectedClist.forEach(cLoc => {
-                    const dis = Math.abs(hLoc.r - cLoc.r) + Math.abs(hLoc.c - cLoc.c);
-                    min = Math.min(min, dis);
-                })
-
-                sum += min;
-                // 아직 치킨거리 누적하고있는데 도시거리보다 크면 더이상 의미없음
-                if(sum >= cityDis) {
-                    sum = -1;
-                    break;
-                }
+    // 각 치킨집에서 모든 집을 돌면서 각 집의 치킨 거리 구하기
+    selected.forEach(cIdx => {
+        const cLoc = Chicken[cIdx];
+        House.forEach((hLoc, hIdx) => {
+            const d = Math.abs(cLoc.r - hLoc.r) + Math.abs(cLoc.c - hLoc.c);
+            if(dis[hIdx] > d) {
+                dis[hIdx] = d;
             }
+        })
+    })
 
-            // 도시의 치킨거리 구하기
-            if(sum !== -1) {
-                cityDis = Math.min(cityDis, sum);
-            }
-        }
+    // 도시 치킨거리 갱신
+    let cityDis = 0;
+    dis.forEach(el => cityDis += el)
+    MinCityDis = Math.min(MinCityDis, cityDis);
+}
+
+const combi = (start, nth, selected) => {
+    if(nth === M) {
+        // 각 치킨집 돌면서 모든 집의 치킨 거리 측정
+        measure(selected);
+
         return;
     }
 
-    isSelected[nth] = true;
-    combi(nth + 1, [...isSelected], m + 1);
-
-    isSelected[nth] = false;
-    combi(nth + 1, [...isSelected], m);
+    for(let i=start; i<chickenLen; i++) {
+        selected[nth] = i;
+        combi(i+1, nth+1, [...selected]);
+    }
 }
 
 const main = () => {
-    const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/stdin' : `${__dirname}/input.txt`).toString().trim().split('\n');
+    const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-    [N, M] = input.shift().split(' ').map(el => +el);
-    Map = input.map(row => row.split(' ').map(el => +el));
+    [N, M] = input.shift().split(' ').map(Number);
+    Map = input.map(row => row.split(' ').map(Number));
 
-    cList = [];
-    hList = [];
+    // 모든 치킨집 위치와 집 위치 넣은 배열 만들기
     Map.forEach((row, r) => row.forEach((el, c) => {
-        // 치킨집 위치 리스트 저장
-        if(el === CHICKEN) {
-            cList.push(new Loc(r, c));
-        }
-        // 집 위치 리스트 저장
-        else if(el === HOME) {
-            hList.push(new Loc(r, c));
-        }
-    }));
-    cLen = cList.length;
+        if(el === 1) House.push(new Loc(r, c));
+        else if(el === 2) Chicken.push(new Loc(r, c));
+    }))
+    houseLen = House.length;
+    chickenLen = Chicken.length;
 
-    // 폐업안시키는 치킨집 고르기
-    combi(0, new Array(cLen).fill(false), 0);
-
-    console.log(cityDis);
+    // 하고많은 치킨집중에 m개 골라보자
+    combi(0, 0, new Array(M).fill(0));
+    
+    console.log(MinCityDis);
 }
 
 main();
